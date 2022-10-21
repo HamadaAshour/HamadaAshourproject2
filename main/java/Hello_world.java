f7187c88f8142e44557a50e529b82daf1eec8de7
Hamada <Hamada@git.com>
2022-10-21 15:33:10 +0200

    the 2 mm

---
+++ main/java/Hello_world.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FuTuRe
+ */
+public class Hello_world {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
